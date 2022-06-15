53b69146e713d7bc0a9b3dcccc70a4cb5c77edd3
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-06-16 00:38:03 +0530

    done

---
+++ 375 Question DSA Sheet/3-chocolate_distribution.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int mindiff(int arr[],int size,int m){
+    if(m==0||size==0){
+        return 0;
+    }
+    sort(arr,arr+size);
+   
+    if(size<m){
+        return -1;
+    }
+    int mind=INT_MAX;
+
+    for(int i=0;i+m-1<size;i++){
+        int d=arr[i+m-1]-arr[i];
+        if(d<mind){
+            mind=d;
+        }
+    }
+        return mind;
+}
+
+int main(){
+    int arr[]={7,3,4,2,9,12,56};
+    // int arr[]={2,3,4,7,9,12,56};
+    
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<mindiff(arr,n,3);
+    return 0;
+}
