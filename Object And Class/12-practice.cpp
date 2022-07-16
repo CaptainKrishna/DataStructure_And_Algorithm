765a6c3831d82558ff682f0411b7c7ae70bff356
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-07-16 23:55:02 +0530

    Done

--- Object And Class/12-practice.cpp
+++ Object And Class/12-practice.cpp
@@ -89,39 +89,40 @@
 // }
 
 // *****************************
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int size;
-    cout<<"Enter the Size of Array:"<<endl;
-    cin>>size;
-    // Array Memory size Allocation Usong Pointer
-    int* arr=new int[size];
-    // Input Data
-    cout<<"Enter the Element of Array"<<endl;
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-    // Print Data
-    cout<<"Your Data is"<<endl;
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<endl;
-    }
-    // Free Array Memory And Delete the Array
-    delete arr;
-    // Not working Code Find The Solution Of the Givin Concept
-    // Input Data
-    cout<<"Enter the Element of Array"<<endl;
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-    // Print Data
-    cout<<"Your Data is"<<endl;
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<endl;
-    }
-return 0;
-}
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int size;
+//     cout<<"Enter the Size of Array:"<<endl;
+//     cin>>size;
+//     // Array Memory size Allocation Usong Pointer
+//     int* arr=new int[size];
+//     // Input Data
+//     cout<<"Enter the Element of Array"<<endl;
+//     for(int i=0;i<size;i++){
+//         cin>>arr[i];
+//     }
+//     // Print Data
+//     cout<<"Your Data is"<<endl;
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i]<<endl;
+//     }
+//     // Free Array Memory And Delete the Array
+//     delete arr;
+//     // Not working Code Find The Solution Of the Givin Concept
+//     // Input Data
+//     cout<<"Enter the Element of Array"<<endl;
+//     for(int i=0;i<size;i++){
+//         cin>>arr[i];
+//     }
+//     // Print Data
+//     cout<<"Your Data is"<<endl;
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i]<<endl;
+//     }
+// return 0;
+// }
+// **************************************************
 
 
 
